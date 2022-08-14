fb5243b461058d7a15fae0eb209315d937174f25
Bengeta <inovozhilov08@gmail.com>
2022-08-15 00:06:18 +1000

    veco: animation of loading pictures, string fix

---
+++ veco/src/main/java/com/veco/vecoapp/android/ui/enums/ResultState.kt
@@ -0,0 +1,9 @@
+package com.veco.vecoapp.android.ui.enums
+
+enum class ResultState {
+    LOADING,
+    SUCCESS,
+    ERROR,
+    CANCELED,
+    None
+}
--- veco/src/main/java/com/veco/vecoapp/android/ui/screen/confimation/confirmation.kt
+++ veco/src/main/java/com/veco/vecoapp/android/ui/screen/confimation/confirmation.kt
@@ -4,6 +4,9 @@ import android.net.Uri
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,22 +22,30 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -42,11 +53,13 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.veco.vecoapp.MR
 import com.veco.vecoapp.android.R
 import com.veco.vecoapp.android.ui.component.misc.VecoButton
+import com.veco.vecoapp.android.ui.enums.ResultState
 
 @Composable
 fun ConfirmationRoute() {
@@ -97,17 +110,20 @@ fun Instruction(instruction: String, warning: String) {
 
 @Composable
 fun ImagePicker() {
+    var result = remember {
+        mutableStateListOf<ResultState>()
+    }
     var imageUri = remember {
-        mutableStateListOf<Uri?>(null)
+        mutableStateListOf<Uri?>()
     }
     repeat(5) {
         imageUri.add(null)
+        result.add(ResultState.None)
     }
 
     var imageCount = remember {
         mutableStateOf(0)
     }
-
     val imagePicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
         onResult = { uri ->
@@ -116,18 +132,26 @@ fun ImagePicker() {
                     imageUri.add(null)
                 }
                 imageUri[imageCount.value] = uri
+                result[imageCount.value] = ResultState.LOADING
                 imageCount.value++
             }
         }
     )
-    PhotoGrid(photos = imageUri, imagePicker = imagePicker, imageCount = imageCount)
+
+    PhotoGrid(
+        photos = imageUri,
+        imagePicker = imagePicker,
+        imageCount = imageCount,
+        result = result
+    )
 }
 
 @Composable
 fun PhotoGrid(
     photos: SnapshotStateList<Uri?>,
     imagePicker: ManagedActivityResultLauncher<String, Uri?>,
-    imageCount: MutableState<Int>
+    imageCount: MutableState<Int>,
+    result: SnapshotStateList<ResultState>
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
@@ -143,40 +167,57 @@ fun PhotoGrid(
                     .size(104.dp, 104.dp)
                     .clip(RoundedCornerShape(10.dp))
             ) {
-                AddImg(imagePicker = imagePicker, imageCount = imageCount.value)
+                AddImg(imagePicker = imagePicker, imageCount = imageCount)
             }
         }
-        items(photos) { photo ->
+        itemsIndexed(photos) { i, photo ->
             if (photo != null) {
                 Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
                         .size(104.dp, 104.dp)
                         .clip(RoundedCornerShape(10.dp))
                         .background(color = Color.Gray)
                 ) {
                     AsyncImage(
+                        modifier = Modifier.alpha(if (result[i] == ResultState.LOADING) 0.5f else 1f),
                         contentScale = ContentScale.Inside,
                         model = photo,
-
                         contentDescription = "Selected image"
                     )
-                    Image(
-                        painter = painterResource(R.drawable.ic_delete_button),
-                        contentDescription = "Delete image",
-                        modifier = Modifier
-                            .width(20.dp)
-                            .height(20.dp)
-                            .align(Alignment.TopEnd)
-                            .padding(4.dp)
-                            .clickable(
-                                enabled = true,
-                                onClickLabel = "Clickable image",
-                                onClick = {
+                    when (result[i]) {
+                        ResultState.LOADING -> {
+                            Box(modifier = Modifier.padding(32.dp)) {
+                                CircularProgressBar(
+                                    percentage = 1f,
+                                    imageCount = imageCount,
+                                    result = result
+                                )
+                            }
+                        }
+                        ResultState.SUCCESS -> {
+                            Box(modifier = Modifier.align(Alignment.TopEnd)) {
+                                CornerImage(id = R.drawable.ic_delete_button) {
                                     photos.removeAt(photos.indexOf(photo))
                                     imageCount.value--
                                 }
-                            )
-                    )
+                            }
+                        }
+                        ResultState.ERROR -> {
+                            ImageButton(R.drawable.ic_resend_img) {
+                                result[i] = ResultState.LOADING
+                            }
+                            Box(modifier = Modifier.align(Alignment.TopEnd)) {
+                                CornerImage(id = R.drawable.ic_error_button) {}
+                            }
+                        }
+                        ResultState.CANCELED -> {
+                            photos.removeAt(i)
+                            imageCount.value--
+                            result[i] = ResultState.None
+                        }
+                        else -> {}
+                    }
                 }
             }
         }
@@ -184,10 +225,26 @@ fun PhotoGrid(
 }
 
 @Composable
-fun AddImg(imagePicker: ManagedActivityResultLauncher<String, Uri?>, imageCount: Int) {
+fun CornerImage(id: Int, func: () -> Unit) {
+    Box(modifier = Modifier.clickable { func() })
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = "Delete image",
+        modifier = Modifier
+            .width(20.dp)
+            .height(20.dp)
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun AddImg(
+    imagePicker: ManagedActivityResultLauncher<String, Uri?>,
+    imageCount: MutableState<Int>
+) {
     Button(
         onClick = {
-            if (imageCount < 5) {
+            if (imageCount.value < 5) {
                 imagePicker.launch("image/*")
             }
         },
@@ -203,6 +260,86 @@ fun AddImg(imagePicker: ManagedActivityResultLauncher<String, Uri?>, imageCount:
             Text(text = "Загрузить")
         }
     }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    /*number: Int,
+    frontSize: TextUnit = 28.sp,*/
+    radius: Dp = 40.dp,
+    color: Color = Color.White,
+    strokeWidth: Dp = 4.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 500,
+    result: SnapshotStateList<ResultState>,
+    imageCount: MutableState<Int>
+
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    var curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+    if (curPercentage.value == percentage) {
+        result[imageCount.value - 1] = ResultState.SUCCESS
+    }
+
+    LaunchedEffect(key1 = true) {
+        if (!animationPlayed) {
+            animationPlayed = true
+        }
+    }
+    var func = {
+        result[imageCount.value - 1] = ResultState.CANCELED
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+
+        modifier = Modifier
+            .size(radius)
+            .clip(CircleShape)
+    ) {
+        ImageButton(R.drawable.ic_cross, func)
+        Canvas(
+            modifier = Modifier
+                .size(radius)
+                .align(Alignment.Center)
+                .padding(4.dp)
+        ) {
+            drawArc(
+                color = color,
+                -90f,
+                360f * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageButton(id: Int, func: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .alpha(0.7f)
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .clickable {
+                func()
+            }
+    )
+    Icon(
+        painter = painterResource(id = id),
+        contentDescription = "cross",
+        tint = Color.White
+    )
 }
 
 @Preview
