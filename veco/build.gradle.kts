7ef2de275943854e26f9355fe802cb4514e0a98c
mikairyuu <miva200267@gmail.com>
2022-08-25 16:39:58 +0300

    android: implement minimal splash screen

--- veco/build.gradle.kts
+++ veco/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-splashscreen:1.0.0")
 
     // Compose
     implementation("androidx.compose.ui:ui:$composeVersion")
--- veco/src/main/java/com/veco/vecoapp/android/ui/MainActivity.kt
+++ veco/src/main/java/com/veco/vecoapp/android/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.veco.vecoapp.android.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import java.io.BufferedReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         testMaterialText = assets.open("material_text.mat")
